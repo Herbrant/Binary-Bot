8c159691be929b29af6fd056434d60e2063a7950
davide.cpp <cdavide98carnemolla@gmail.com>
2018-10-10 22:27:17 +0000

    Deleted unuseful lines.

--- functions.py
+++ functions.py
@@ -12,8 +12,6 @@ import re
 # Token
 tokenconf = open('config/token.conf', 'r').read()
 tokenconf = tokenconf.replace("\n", "")
-with open('config/settings.yaml', 'r') as yaml_config:
-    config_map = yaml.load(yaml_config)
 
 # Token of your telegram bot that you created from @BotFather, write it on token.conf
 TOKEN = tokenconf
--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from functions import *
 
 bot = telegram.Bot(TOKEN)
 
-with open('config/settings.yaml') as yaml_config:
-	config_map = yaml.load(yaml_config)
-
-
 def main():
 	updater = Updater(TOKEN)
 	dp = updater.dispatcher
