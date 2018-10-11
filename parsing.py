d624c360cc76f8bafa81b11df26d2ebc33a9beab
davide.cpp <cdavide98carnemolla@gmail.com>
2018-10-11 21:18:16 +0000

    Update.

--- parsing.py
+++ parsing.py
@@ -1,5 +1,46 @@
 import re
 
+#Binary sum
+def binary_sum(a, b):
+    len_max = max(len(a), len(b))
+
+    a = a.zfill(len_max)
+    b = b.zfill(len_max)
+
+    result = ''
+    carry = 0
+
+    for i in range(len_max - 1, -1, -1):
+        r = carry
+        if a[i] == '1':
+            r += 1
+        if b[i] == '1':
+            r += 1
+        if(r % 2 == 1):
+            result += 1
+        if r < 2:
+            carry = 0
+        else:
+            carry = 1
+    if carry != 0:
+        result = '1' + carry
+    return result.rzfill(len_max)
+
+#Binary subtraction
+def binary_sub(a, b):
+    result = ''
+    carry = 0
+
+    for i in range(len_max - 1, -1, -1):
+        r = carry
+        if a[i] == '1' and b[i] == '1':
+            result += '0'
+        elif a[i] == '1' and b[i] == '0':
+            result += '1'
+        elif a[i] == '0' and b[i] == '0':
+            result += '0'
+        elif a[]
+
 #Function that controls if str is a binary
 def is_binary(str):
     if re.match("^[0-1]*$", str):
@@ -22,9 +63,9 @@ def apply_operator(operators, values):
     right = values.pop()
     left = values.pop()
     if operator == '+':
-        values.append(sum(left, right))
+        values.append(binary_sum(left, right))
     elif operator == '-':
-        values.append(diff(left, right))
+        values.append(binary_sub(left, right))
     elif operator == '*':
         values.append(multy(left, right))
     elif operator == '/':
