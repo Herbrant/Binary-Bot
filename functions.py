b536ba8b9283240a06f57fd2d69ebdd7625585b2
davide.cpp <cdavide98carnemolla@gmail.com>
2018-10-11 16:47:08 +0000

    Add parsing and fix function.

--- functions.py
+++ functions.py
@@ -83,6 +83,33 @@ def dec_to_binary(n):
 def binary_to_dec(n):
     return int(n,2)
 
+#Binary sum
+def binary_sum(a, b):
+    len_max = max(len(a), len(b))
+
+    a = a.zfill(len_max)
+    b = b.zfill(len_max)
+
+    result = ''
+    carry = 0
+
+    for i in range(len_max - 1, -1, -1):
+        r = carry
+        if a[i] == '1':
+            r += 1
+        if b[i] == '1':
+            r += 1
+        if(r % 2 == 1):
+            result += 1
+        if r < 2:
+            carry = 0
+        else:
+            carry = 1
+    if carry != 0:
+        result = '1' + carry
+    return result.rzfill(len_max)
+
+
 
 #Bot Command
 
---
+++ parsing.py
@@ -0,0 +1,54 @@
+import re
+
+def is_binary(str):
+    if re.match("^[0-1]*$", str):
+        return True
+    return False
+
+def is_name(str):
+    if re.match("\W+", str):
+        return True
+    return False
+
+def peek(stack):
+    return stack[-1] if stack else None
+ 
+def apply_operator(operators, values):
+    operator = operators.pop()
+    right = values.pop()
+    left = values.pop()
+    
+    values.append(eval("{0}{1}{2}".format(left, operator, right)))
+ 
+def greater_precedence(op1, op2):
+    precedences = {'+' : 0, '-' : 0, '*' : 1, '/' : 1}
+    return precedences[op1] > precedences[op2]
+ 
+def evaluate(expression):
+    tokens = re.findall("[+\/*()-]|[0-1]+", expression)
+    values = []
+    operators = []
+    for token in tokens:
+        if is_binary(token):
+            values.append(int(token))
+        elif token == '(':
+            operators.append(token)
+        elif token == ')':
+            top = peek(operators)
+            while top is not None and top != '(':
+                apply_operator(operators, values)
+                top = peek(operators)
+            operators.pop() # Discard the '('
+        else:
+            # Operator
+            top = peek(operators)
+            while top is not None and top not in "()" and greater_precedence(top, token):
+                apply_operator(operators, values)
+                top = peek(operators)
+            operators.append(token)
+    while peek(operators) is not None:
+        apply_operator(operators, values)
+ 
+    return values[0]
+
+
