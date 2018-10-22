fe043ace5ab718202176832b657baa4e62ddd18c
davide.cpp <cdavide98carnemolla@gmail.com>
2018-10-22 10:29:12 +0000

    Fix text.

--- functions.py
+++ functions.py
@@ -20,7 +20,7 @@ def dec_to_binary_cmd(bot, update, args):
     if(re.match('^[-|+]?[0-9]+$', args[0])):        #Regex that check input.
         message = dec_to_two(args[0])
     else:
-        message = "Sintax error. Type /help to more information."
+        message = "Syntax error. Type /help to more information."
     bot.sendMessage(chat_id=update.message.chat_id, text = message)
 
 #Binary to decimal command
@@ -28,12 +28,12 @@ def binary_to_dec_cmd(bot, update, args):
     if(re.match('^[0-1]+$', args[0])):       #Regex that check input.
         message = binary_to_dec(str(args[0]))
     else:
-        message = "Sintax error. Type /help to more information."
+        message = "Syntax error. Type /help to more information."
     bot.sendMessage(chat_id=update.message.chat_id, text = message)
 
 #Start command
 def start_cmd(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Hi! I'm Binary Bot and I can manipulate binary number. If you want to know what you can do, use /help")
+    bot.sendMessage(chat_id=update.message.chat_id, text="Hi! I'm Binary Bot and I can manipulate binary numbers. If you want to know what you can do, use /help")
 
 
 #Help command
@@ -43,7 +43,7 @@ def help_cmd(bot, update):
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    update.message.reply_text('@Binary_Bot commands:', reply_markup = reply_markup)
+    update.message.reply_text('@binarydmibot commands:', reply_markup = reply_markup)
 
 def button(bot, update):
     query = update.callback_query
@@ -51,7 +51,7 @@ def button(bot, update):
     message_id = query.message.message_id
     data = query.data
 
-    if data == "calculate":
+    if data == "calculate":Sintax
         bot.editMessageText(text = "Type /calculate with expression that you want to calculate (e.g /calculate 1011+010).", chat_id = chat_id, message_id = message_id)
     elif data == "bin":
         bot.editMessageText(text = "Type /bin with the decimal number that you want to convert to two's complement binary number (e.g /bin 2).", chat_id = chat_id, message_id = message_id)
@@ -64,6 +64,6 @@ def calculate_cmd(bot, update, args):
         val = evaluate(str(args[0]))
         message = str(val) + " (" + str(binary_to_dec(val)) + ")"
     else:
-        message = "Sintax error. Type /help to more information."
+        message = "Syntax error. Type /help to more information."
 
     bot.sendMessage(chat_id = update.message.chat_id, text = message)
